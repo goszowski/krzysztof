2f2c63fe55291795fbe9bf871b45a14a7a113e74
Jaroslaw Goszowski <goszowski@gmail.com>
2017-12-13 18:47:24 +0200

    prototyp

---
+++ src/slider.js
@@ -0,0 +1,34 @@
+function build() {
+
+	console.log('build');
+
+	var scope = $('.scope.active');
+
+	var track = $('.track');
+
+	// console.log(scope[0].scrollHeight, track.height());
+
+	while(scope[0].scrollHeight > track.height())
+	{
+		console.log('object moved');
+		$('.block').first().clone().appendTo('.scope-2');
+		$('.block').first().remove();
+	}
+
+	var scopeActive = 1;
+	var scopesCount = 2;
+
+	setInterval(function() {
+
+		scopeActive += 1;
+
+		if(scopeActive > scopesCount)
+		{
+			scopeActive = 1;
+		}
+
+		$('.scope').removeClass('active');
+		$('.scope-'+scopeActive).addClass('active');
+
+	}, 1000);
+}
